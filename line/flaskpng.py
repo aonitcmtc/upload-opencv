29bed46574e4f94c4ace4001290ec82141cfc2dc
aonitcmtc <47767084+aonitcmtc@users.noreply.github.com>
2020-12-02 08:51:12 +0700

    add line pyrhon

---
+++ line/flaskpng.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, send_file
+app = Flask(__name__)
+
+@app.route('/get_image')
+def get_image():
+    if request.args.get('type') == '1':
+       filename = 'ok.gif'
+    else:
+       filename = 'error.gif'
+    return send_file(filename)
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0', threaded=True)
+
